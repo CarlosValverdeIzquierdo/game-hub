72bdd87308d01fcce2d2c13438208f2d48e941f8
CarlosValverdeIzquierdo <vaiz.carlos@gmail.com>
2024-04-25 13:52:12 +0200

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImageUrl from '../services/image-urls'
+import React from 'react';
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/image-urls';
 
 interface Props {
-  game: Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
@@ -14,16 +14,16 @@ const GameCard = ({ game }: Props) => {
     <Card width="100%" borderRadius={10} overflow={'hidden'}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize={'2xl'}>{game.name}</Heading>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,17 @@
-import { SimpleGrid, Text } from '@chakra-ui/react'
-import useGames, { Platform } from '../hooks/useGames'
-import GameCard from './GameCard'
-import GameCardSkeleton from './GameCardSkeleton'
-import GameCardContainer from './GameCardContainer'
-import { GameQuery } from '../App'
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames, { Platform } from '../hooks/useGames';
+import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
+import GameCardContainer from './GameCardContainer';
+import { GameQuery } from '../App';
 
 interface Props {
-  gameQuery: GameQuery
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery)
-  const skeletons = [1, 2, 3, 4, 5, 6]
+  const { data, error, isLoading } = useGames(gameQuery);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -19,7 +19,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
@@ -34,7 +34,7 @@ const GameGrid = ({ gameQuery }: Props) => {
         ))}
       </SimpleGrid>
     </>
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
